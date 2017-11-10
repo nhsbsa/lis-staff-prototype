320922c1529b49746acd27175e47e3376085e85d
Tim Gorringe <t.gorringe@nhs.net>
2017-11-10 16:58:32 +0000

    added drop down from nav bar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -68,5 +68,32 @@ $(document).ready(function(){
     });
 
 });
+    
+    $(function(){
+  
+  $('li.dropdown > a').on('click',function(event){
+    
+    event.preventDefault()
+    
+    $(this).parent().find('ul').first().toggle(300);
+    
+    $(this).parent().siblings().find('ul').hide(200);
+    
+    //Hide menu when clicked outside
+    $(this).parent().find('ul').mouseleave(function(){  
+      var thisUI = $(this);
+      $('html').click(function(){
+        thisUI.hide();
+        $('html').unbind('click');
+      });
+    });
+    
+    
+  });
+  
+
+ 
+  
+});
   
 })
